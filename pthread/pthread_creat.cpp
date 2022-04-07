c999ab614c80784a98f86e2c4e21be6c23ae0d5b
YRZ9527 <44866789+YRZ9527@users.noreply.github.com>
2022-04-07 22:07:33 +0800

    4.7

--- pthread/pthread_creat.cpp
+++ pthread/pthread_creat.cpp
@@ -5,16 +5,35 @@ void *fun(void *arg)
     cout<<"new pthread "<<pthread_self()<<"\n";
     return NULL;
 }
+
+
+void *fun_2(void *arg)
+{
+    int var  = (int)(long)arg;
+    cout<<"线程2 var = "<<var<<"\n";
+    return NULL;
+}
 int main(int argc, char **argv)
 {
     int ret = -1;
-    pthread_t tid = -1;
+    pthread_t tid ;
+    pthread_t tid_2;
 
     ret = pthread_create(&tid, NULL,fun,NULL);
     if(0 != ret)
     {
         cout<<"pthread create failed"<<endl;
         return 1;
+    }
+
+
+    ret = pthread_create(&tid_2,NULL,fun_2,(void*)0x3);
+    {
+        if(0 != ret)
+        {
+            cout<<"pthread_create failed"<<endl;
+            return 1;
+        }
     }
     cout<<"main pthread tid"<<pthread_self()<<endl;
 
