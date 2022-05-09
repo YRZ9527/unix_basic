de541f4edc781fabfc8bfee4204e8150ac45ab04
YRZ9527 <44866789+YRZ9527@users.noreply.github.com>
2022-05-09 23:24:26 +0800

    5.9

--- DSA/List/ListNode.cpp
+++ DSA/List/ListNode.cpp
@@ -70,3 +70,59 @@ public:
     template <typename VST>
     void traverse(VST &);
 };
+
+template <typename T>
+void List<T>::init()
+{
+    header = new ListNode<T>;
+    trailer = new ListNode<T>;
+    header->succ = trailer;
+    header->pred = nullptr;
+    trailer->pred = header;
+    trailer->succ = nullptr;
+    _size = 0;
+}
+
+template <typename T>
+T& List<T>::operator[](int r)const
+{
+    ListNodePosi(T) p = first();
+    while(0 < r--)
+    p = p->succ;
+    return p->data;
+}
+
+
+template<typename T>
+ListNodePosi(T) List<T>::find(T const& e, int n, ListNodePosi(T) p)const
+{
+    while(0 < n--)
+        if(e == (p = p->pred)->data)
+        return p;
+    return nullptr;
+}
+
+template<typename T>
+ListNodePosi(T) List<T>::insertAsFirst(T const& e)
+{
+    _size++;
+    return header->insertAsSucc(e);
+}
+template<typename T>
+ListNodePosi(T) List<T>::insertAsLast(T const& e)
+{
+    _size++;
+    return header->insertAspred(e);
+}
+template<typename T>
+ListNodePosi(T) List<T>::insertBefore(ListNodePosi(T) p, T const& e)
+{
+    _size++;
+    return p->insertAsPred(e);
+}
+template <typename T>
+ListNodePosi(T) List<T>::insertAfter(ListNodePosi(T) p, T const& e)
+{
+    _size++;
+    return p->insertAsSucc(e);
+}
