efff6cbeb6f248392515ebad9fd637e4dd7e871d
YRZ9527 <44866789+YRZ9527@users.noreply.github.com>
2022-07-08 23:38:26 +0800

    test

---
+++ basic/plot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+import numpy as np
+nums = []
+tmp1 = [60,60,60,60,60,60,57.5,61,63.5,62,45,35,27,21,16,12,9.5,7.7,6.4,4.6,4.1,3.7,2.2,2.2,2.0,1.9,1.3,1.2,1.1,1.0,0.8,0.5];
+
+
+for i in range(tmp1):
+    nums.append(i)
+
+
+
+# int main() {
+#     vector<float> res;
+#     vector<float> tmp = {66,66,66,66,66,65.6,65.5,65,65,62,45,37,31,27,23,20,18,15,12,9.5,7.5,6.1,5.5,4.8,4.2,3.8,3.2,2.8,2.4,2.3,2.0,1.6,1.0,1.7,1.2,1.7};
+#     vector<float> tmp1 = {60,60,60,60,60,60,57.5,61,63.5,62,45,35,27,21,16,12,9.5,7.7,6.4,4.6,4.1,3.7,2.2,2.2,2.0,1.9,1.3,1.2,1.1,1.0,0.8,0.5};
+
+#     for(int i = 0; i < tmp1.size(); i++)
+#     {
+#         res.push_back(tmp1[i] / 70);
+#         cout<<res[i]<<endl;
+#     }
+#     cout<<tmp1.size()<<endl;
+    
+# }
