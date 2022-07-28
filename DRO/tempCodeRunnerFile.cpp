5f542a2242ed723490940561f4dbf8e49eb21025
YRZ9527 <44866789+YRZ9527@users.noreply.github.com>
2022-07-28 12:38:45 +0800

    test

--- DRO/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
- // for(int i = 0 ; i < tmpStr.size(); i+=8)
-            // {
-            //     // vectorResult.push_back(tmpStr.substr(i,8));
-            //     cout<<tmpStr.substr(i,8)<<endl;
-            // }
